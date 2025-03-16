53b5f7ac4180f98130c6365095e3551a32d849cb
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-03-15 22:45:13 -0400

    fix: remove unecessary checks in default parse

--- src/helpers.ts
+++ src/helpers.ts
@@ -7,13 +7,6 @@ export function defaultParse(unparsed: string): unknown {
   // JSON.parse errors on "undefined"
   if (unparsed === "undefined") return undefined;
 
-  // Number parses "" to 0
-  if (unparsed === "") return "";
-
-  // Number coerces bigints to numbers
-  const maybeNum = Number(unparsed);
-  if (!Number.isNaN(maybeNum)) return maybeNum;
-
   try {
     return JSON.parse(unparsed);
   } catch {
